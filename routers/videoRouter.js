8bbeb8c3c8fbc2b2e2914d63905ae94313710b73
Rhange <rhange0511@gmail.com>
2020-02-13 16:24:16 +0900

    update routes
    1. added 'routes.js'
    2. it is a contrainer routes that has global, users, and videos routes.

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,13 @@
 import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
+
+globalRouter.get(routes.videos, (req, res) => res.send("Videos"));
+globalRouter.get(routes.upload, (req, res) => res.send("Upload"));
+globalRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+globalRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
+globalRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
 
 //? export const ~ 는 '변수'만 내놓는 것
 //! export default는 '파일'을 내놓는 것
