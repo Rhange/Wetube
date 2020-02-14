fcc08d54675eb46df9e59766d70ca958d3c1f2a1
Rhange <rhange0511@gmail.com>
2020-02-14 14:05:06 +0900

    update middlewares.js
    1. app.js
     - add app.use(localMiddleware)
     - localMiddleware is a function that is in middlewares.js
    
    2. middlewares.js
     - import routes from routes.js
     - localsMiddleware function
      - has global valuables (siteName, routes)
    
    3. set global valuables to main / header / footer pug files
     - main & footer : 'WeTube' -> #{siteName}
     - header :  In 'a(href="#")', # = routes.home, routes.join, and routes.login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,15 +11,16 @@ import routes from "./routes";
 
 const app = express();
 
+//TODO middleware helmet, morgan, cookie-parser, body-parser(json, urlencoded)
+app.use(helmet());
 //? PUG
 app.set("view engine", "pug");
-
-//TODO middleware helmet, morgan, cookie-parser, body-parser(json, urlencoded)
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware); //! locals 위에 있는 것은 locals에 접근 X
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
