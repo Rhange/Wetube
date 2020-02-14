543fa064625eca2f8d62b7464bf967c043a6667f
Rhange <rhange0511@gmail.com>
2020-02-14 14:22:58 +0900

    update local_variables
    1. res.render("join", { variable_name: variable_value });

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,7 @@
 import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
+  //TODO res.locals.variables_name = "variables_value";
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   next(); //! middlewares are between codes so to next to the function.
