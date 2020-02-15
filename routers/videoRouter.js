e309eb467de91f096944b46506fc005101197607
Rhange <rhange0511@gmail.com>
2020-02-15 18:16:26 +0900

    fixed useless codes
    - removed 'user-route' and 'video-route'

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-  videos,
   upload,
   videoDetail,
   editVideo,
@@ -10,11 +9,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 //? export const ~ 는 '변수'만 내놓는 것
 //! export default는 '파일'을 내놓는 것
