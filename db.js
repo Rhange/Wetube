30fc2654af07c0737469f052dd818962631f3433
Rhange <rhange0511@gmail.com>
2020-02-15 18:21:37 +0900

    update home.pug
    1. made videos  from 'fake DB'
    2. input above videos to home
    3. made 'mixins' to re-use whenever
    4. at home.pug, input video info by using iteration.

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 123413,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 435345,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 123643,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 435345,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 1213263,
+    title: "Video Nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 435345,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 4456234,
+    title: "Video Beautiful",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 435345,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+];
