f052f0622d9953c62dea78d548a044559e62aee3
Rhange <rhange0511@gmail.com>
2020-02-15 20:04:23 +0900

    update upload
    1. if click upload on the header, go to upload page.
    2. if click upload button, go to the uploaded video page.

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import routes from "../routes";
 import {
-  upload,
+  getUpload,
+  postUpload,
   videoDetail,
   editVideo,
   deleteVideo
@@ -10,9 +11,10 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 videoRouter.get(routes.deleteVideo, deleteVideo);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 //? export const ~ 는 '변수'만 내놓는 것
 //! export default는 '파일'을 내놓는 것
