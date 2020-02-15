d15aad182828f4399e1e0d896565397e6445517c
Rhange <rhange0511@gmail.com>
2020-02-15 19:40:44 +0900

    update header
    1. made fake ID
    2. when this user is logging, routes have user's id

--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,9 @@ export const localsMiddleware = (req, res, next) => {
   //TODO res.locals.variables_name = "variables_value";
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  };
   next(); //! middlewares are between codes so to next to the function.
 };
