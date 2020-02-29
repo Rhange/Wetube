044036d9607b9a8a23ed51bef47aad9a7fcf65ca
Rhange <rhange0511@gmail.com>
2020-02-29 17:54:03 +0900

    Update videoController.js
    1. update videoDetail function.
     - apply async & await to get video info from the video's id.
     - send pageTitle and video info to videoDetail.pug
     - if occur error, redirect to home.pug
    
    2. update editVideo function
     1) get
      - get video information by using async & await and findById(req.params.id)
     2) post
      - update video info ( title and description ) by using findOneAndUpdate ( it is from 'Mongoose' ).
      - it is needed to fix, it can't save updated title and description.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -13,7 +13,7 @@ export const home = async (req, res) => {
 
 export const search = (req, res) => {
   const {
-    query: { term: searchingBy }
+    query: { term: searchingBy },
   } = req; //! Beautiful
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
@@ -24,23 +24,54 @@ export const getUpload = (req, res) =>
 export const postUpload = async (req, res) => {
   const {
     body: { title, description },
-    file: { path }
+    file: { path },
   } = req;
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
   });
-  console.log(newVideo);
   //TODO: Upload and save video
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    console.log(error);
+    res.redirect(routes.home);
+  }
+};
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description },
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
