311001e98ab21946cd0bd152a6f4559aa41deeba
Rhange <rhange0511@gmail.com>
2020-03-27 20:12:35 +0900

    Update videoController.js
    added delete comments API
    
    - In this API, update User and Video's comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -43,7 +43,6 @@ export const postUpload = async (req, res) => {
   });
   req.user.videos.push(newVideo.id);
   req.user.save();
-  // TODO: Upload and save video
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -51,10 +50,14 @@ export const videoDetail = async (req, res) => {
   const {
     params: { id }
   } = req;
+
+  console.log(req.user);
+
   try {
     const video = await Video.findById(id)
       .populate("creator")
       .populate("comments");
+    // console.log(String(video.comments[0].creator));
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
@@ -95,14 +98,21 @@ export const postEditVideo = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const {
-    params: { id }
+    params: { id },
+    user
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("comments");
     if (String(video.creator) !== req.user.id) {
       throw Error();
     } else {
-      await Video.findOneAndRemove({ _id: id });
+      console.log(user.comments.length);
+      user.videos = user.videos.filter(each => String(each) !== video.id);
+      user.save();
+      // const commentCreatorArray = video.comments.map(each => each.creator);
+      // TODO: How to make if video were deleted, all user's comments have to remove from their user comments DB
+      console.log(user.comments.length);
+      await Video.findOneAndDelete({ _id: id });
     }
   } catch (error) {
     console.log(error);
@@ -137,13 +147,68 @@ export const postAddComment = async (req, res) => {
     user
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("comments");
+
     const newComment = await Comment.create({
       text: comment,
-      creator: user.id
+      creator: user.id,
+      delete: "✖"
     });
+
+    // const checkCommentsCreator = await video.comments.map(each => each.creator);
+    // console.log(checkCommentsCreator);
+
     video.comments.push(newComment.id);
     video.save();
+
+    user.comments.push(newComment.id);
+    user.save();
+
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+//! Remove Comment
+
+export const postDeleteComment = async (req, res) => {
+  //! From API get clicked target button's 'commentId'
+  // console.log(commentId);
+  const {
+    params: { id },
+    body: { commentId },
+    user
+  } = req;
+
+  try {
+    const video = await Video.findById(id).populate("comments");
+
+    //! Update Video model's comments
+    const videoCommentsIdArray = video.comments.map(each => each.id);
+    // console.log(videoCommentsIdArray);
+    // console.log(typeof videoCommentsIdArray[0]);
+    // console.log(
+    //   videoCommentsIdArray.filter(each => String(each) !== commentId)
+    // );
+    video.comments = videoCommentsIdArray.filter(
+      each => String(each) !== commentId
+    );
+    video.save();
+
+    //! Update User model's comments
+    // console.log(user.comments);
+    const userCommentsIdArray = user.comments;
+    // console.log(userCommentsIdArray);
+    // console.log(typeof userCommentsIdArray[0]);
+    // console.log(userCommentsIdArray.filter(each => String(each) !== commentId));
+    user.comments = userCommentsIdArray.filter(
+      each => String(each) !== commentId
+    );
+    user.save();
+
     res.status(200);
   } catch (error) {
     res.status(400);
