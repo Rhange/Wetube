743cd1d96a7186ff894a47a10f31bdde149496fe
Rhange <rhange0511@gmail.com>
2020-03-27 20:11:19 +0900

    update delComment.js
    1. This js activate when commentNumber is exist and the number of that is more than '0'
    
    2. When target is clicked, it send target's parentNode(<li> tag) to API because  <li> has 'comment.id'
    
    3. After API, it send <li> to delComment function which remove <li> from <ul>
    
    4. Finally, activate decreaseNumber function, it decrease commentNumber when comments are removed

---
+++ src/assets/js/delComment.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const commentNumber = document.getElementById("jsCommentNumber");
+const commentList = document.getElementById("jsCommentList");
+const delBtn = commentList.querySelectorAll("button");
+
+const decreaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) - 1;
+};
+
+const delComment = targetLi => {
+  commentList.removeChild(targetLi);
+  decreaseNumber();
+};
+
+const sendDelComment = async targetLi => {
+  const videoId = window.location.href.split("/videos/")[1];
+  const response = await axios({
+    url: `/api/${videoId}/comment/delete`,
+    method: "POST",
+    data: {
+      commentId: targetLi.className
+    }
+  });
+  if (response.status === 200) {
+    delComment(targetLi);
+  }
+};
+
+const handleClick = e => {
+  console.log(e.target);
+  console.log(e.target.parentNode);
+  const targetLi = e.target.parentNode;
+  sendDelComment(targetLi);
+};
+
+const init = () => {
+  //   console.log(delBtn);
+  delBtn.forEach(each => each.addEventListener("click", handleClick));
+};
+
+if (commentNumber && commentNumber.innerText > 0) {
+  init();
+}
