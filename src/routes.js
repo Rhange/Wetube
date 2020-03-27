8a8ac3f827aeba6179cb3c0c85048de5f93e7317
Rhange <rhange0511@gmail.com>
2020-03-27 20:03:51 +0900

    Update routes.js
    added delete comment route

--- src/routes.js
+++ src/routes.js
@@ -27,6 +27,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
 const ADD_COMMENT = "/:id/comment";
+const DELETE_COMMENT = "/:id/comment/delete";
 
 const routes = {
   home: HOME,
@@ -68,7 +69,8 @@ const routes = {
   me: ME,
   api: API,
   registerView: REGISTER_VIEW,
-  addComment: ADD_COMMENT
+  addComment: ADD_COMMENT,
+  deleteComment: DELETE_COMMENT
 };
 
 export default routes;
